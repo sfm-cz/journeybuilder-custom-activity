6f0f4d174546def2bea30a31973812c1f8885382
Orkhan Alakbarli <alakbarliorkhan@gmail.com>
2020-10-30 14:25:15 +0100

    code polishing & security enhancements

--- public/javascript/customActivity.js
+++ public/javascript/customActivity.js
@@ -1,141 +1,136 @@
-/* eslint-disable */
-define(['postmonger'], function (Postmonger) {
-    'use strict';
+'use strict';
 
-    const validateForm = function(cb) {
-        require(['validate'], function() {
-            $form = $('.js-settings-form');
+const validateForm = function(cb) {
+    $form = $('.js-settings-form');
 
-            $form.validate({
-                submitHandler: function(form) { },
-                errorPlacement: function () { },
-            });
+    $form.validate({
+        submitHandler: function(form) { },
+        errorPlacement: function () { },
+    });
 
-            cb($form);
-        });
-    };
+    cb($form);
+};
 
-    const connection = new Postmonger.Session();
-    let authTokens = {};
-    let payload = {};
-    let $form;
-    $(window).ready(onRender);
+const connection = new Postmonger.Session();
+let authTokens = {};
+let payload = {};
+let $form;
+$(window).ready(onRender);
 
-    connection.on('initActivity', initialize);
-    connection.on('requestedTokens', onGetTokens);
-    connection.on('requestedEndpoints', onGetEndpoints);
+connection.on('initActivity', initialize);
+connection.on('requestedTokens', onGetTokens);
+connection.on('requestedEndpoints', onGetEndpoints);
 
-    connection.on('clickedNext', save);
+connection.on('clickedNext', save);
 
-    const buttonSettings = {
-        button: 'next',
-        text: 'done',
-        visible: true,
-        enabled: false,
-    };
+const buttonSettings = {
+    button: 'next',
+    text: 'done',
+    visible: true,
+    enabled: false,
+};
 
-    function onRender() {
-        connection.trigger('ready');
-        connection.trigger('requestTokens');
-        connection.trigger('requestEndpoints');
+function onRender() {
+    connection.trigger('ready');
+    connection.trigger('requestTokens');
+    connection.trigger('requestEndpoints');
 
-        // validation
-        validateForm(function($form) {
-            $form.on('change click keyup input paste', 'input, textarea', function () {
-                buttonSettings.enabled = $form.valid();
-                connection.trigger('updateButton', buttonSettings);
-            });
-        });
-    }
-
-    /**
-     * Initialization
-     * @param data
-     */
-    function initialize(data) {
-        if (data) {
-            payload = data;
-        }
-        const hasInArguments = Boolean(
-            payload['arguments'] &&
-            payload['arguments'].execute &&
-            payload['arguments'].execute.inArguments &&
-            payload['arguments'].execute.inArguments.length > 0
-        );
-
-        const inArguments = hasInArguments
-            ? payload['arguments'].execute.inArguments
-            : {};
-
-        $.each(inArguments, function (index, inArgument) {
-            $.each(inArgument, function (key, value) {
-                const $el = $('#' + key);
-                if($el.attr('type') === 'checkbox') {
-                    $el.prop('checked', value === 'true');
-                } else {
-                    $el.val(value);
-                }
-            });
-        });
-
-        validateForm(function($form) {
+    // validation
+    validateForm(function($form) {
+        $form.on('change click keyup input paste', 'input, textarea', function () {
             buttonSettings.enabled = $form.valid();
             connection.trigger('updateButton', buttonSettings);
         });
+    });
+}
+
+/**
+ * Initialization
+ * @param data
+ */
+function initialize(data) {
+    if (data) {
+        payload = data;
     }
+    const hasInArguments = Boolean(
+        payload['arguments'] &&
+        payload['arguments'].execute &&
+        payload['arguments'].execute.inArguments &&
+        payload['arguments'].execute.inArguments.length > 0
+    );
 
-    /**
-     *
-     *
-     * @param {*} tokens
-     */
-    function onGetTokens(tokens) {
-        authTokens = tokens;
-    }
+    const inArguments = hasInArguments
+        ? payload['arguments'].execute.inArguments
+        : {};
 
-    /**
-     *
-     *
-     * @param {*} endpoints
-     */
-    function onGetEndpoints(endpoints) {
-        console.log(endpoints);
-    }
+    $.each(inArguments, function (index, inArgument) {
+        $.each(inArgument, function (key, value) {
+            const $el = $('#' + key);
+            if($el.attr('type') === 'checkbox') {
+                $el.prop('checked', value === 'true');
+            } else {
+                $el.val(value);
+            }
+        });
+    });
 
-    /**
-     * Save settings
-     */
-    function save() {
-        if($form.valid()) {
-            payload['metaData'].isConfigured = true;
+    validateForm(function($form) {
+        buttonSettings.enabled = $form.valid();
+        connection.trigger('updateButton', buttonSettings);
+    });
+}
 
-            payload['arguments'].execute.inArguments = [
-                {
-                    "contactKey": "{{Contact.Key}}"
-                }
-            ];
+/**
+ *
+ *
+ * @param {*} tokens
+ */
+function onGetTokens(tokens) {
+    authTokens = tokens;
+}
 
-            $('.js-activity-setting').each(function () {
-                const $el = $(this);
-                const setting = {
-                    id: $(this).attr('id'),
-                    value: $(this).val()
-                };
+/**
+ *
+ *
+ * @param {*} endpoints
+ */
+function onGetEndpoints(endpoints) {
+    console.log(endpoints);
+}
 
-                $.each(payload['arguments'].execute.inArguments, function(index, value) {
-                    if($el.attr('type') === 'checkbox') {
-                        if($el.is(":checked")) {
-                            value[setting.id] = setting.value;
-                        } else {
-                            value[setting.id] = 'false';
-                        }
-                    } else {
+/**
+ * Save settings
+ */
+function save() {
+    if($form.valid()) {
+        payload['metaData'].isConfigured = true;
+
+        payload['arguments'].execute.inArguments = [
+            {
+                "contactKey": "{{Contact.Key}}"
+            }
+        ];
+
+        $('.js-activity-setting').each(function () {
+            const $el = $(this);
+            const setting = {
+                id: $(this).attr('id'),
+                value: $(this).val()
+            };
+
+            $.each(payload['arguments'].execute.inArguments, function(index, value) {
+                if($el.attr('type') === 'checkbox') {
+                    if($el.is(":checked")) {
                         value[setting.id] = setting.value;
+                    } else {
+                        value[setting.id] = 'false';
                     }
-                })
-            });
+                } else {
+                    value[setting.id] = setting.value;
+                }
+            })
+        });
 
-            connection.trigger('updateActivity', payload);
-        }
+        connection.trigger('updateActivity', payload);
     }
-});
+}
